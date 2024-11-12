2366b7081ea7cebf90b3d2d3f20a201abf79fd23
shivanikhalote <shivanikhalote1999@gmail.com>
2024-11-12 23:27:25 +0530

    added vector data

---
+++ apps/vector.py
@@ -0,0 +1,12 @@
+import os
+import geopandas as gpd
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+def app():
+    st.title("Vector")
+    m = leafmap.Map(center=[0, 0], zoom=2)
+
+    in_geojson = "https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/cable_geo.geojson"
+    m.add_geojson(in_geojson, layer_name="Cable lines")
+    m.to_streamlit(height=700)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap, upload  # import your app modules here
+from apps import home, heatmap, upload, vector  # import your app modules here
 
-st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+st.set_page_config(page_title="BTAD Land Degradation", layout="wide")
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
@@ -11,6 +11,7 @@ apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
     {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
     {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+    {"func": vector.app, "title": "vector", "icon": "bounding-box-circles"},
 ]
 
 titles = [app["title"] for app in apps]
@@ -36,12 +37,7 @@ with st.sidebar:
     st.sidebar.title("About")
     st.sidebar.info(
         """
-        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
-            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
-        
-        Source code: <https://github.com/giswqs/streamlit-template>
 
-        More menu icons: <https://icons.getbootstrap.com>
     """
     )
 
